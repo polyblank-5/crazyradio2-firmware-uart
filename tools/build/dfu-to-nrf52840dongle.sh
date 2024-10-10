0d24dec2add3e27edc927a291988b6b388fbaedf
Jan Achermann <achermann@campus.tu-berlin.de>
2024-10-10 14:18:08 +0200

    small changes

--- tools/build/dfu-to-nrf52840dongle.sh
+++ tools/build/dfu-to-nrf52840dongle.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 nrfutil="/home/polyblank/Programms/nrfutil"
-BUILD='build_blinky_dongle' 
+BUILD='build_uart_dongle' 
+PROJECT="crazyradio2"
 $nrfutil pkg generate --hw-version 52 --sd-req=0x00 \
-                     --application $BUILD/zephyr/crazyradio2.hex \
-                     --application-version 1 $BUILD/zephyr/crazyradio2.zip
+                     --application $BUILD/zephyr/$PROJECT.hex \
+                     --application-version 1 $BUILD/zephyr/$PROJECT.zip
 
 echo Using NRF DFU bootloader on /dev/ttyACM0
-$nrfutil dfu usb-serial -pkg $BUILD/zephyr/crazyradio2.zip -p /dev/ttyACM0
+$nrfutil dfu usb-serial -pkg $BUILD/zephyr/$PROJECT.zip -p /dev/ttyACM0
