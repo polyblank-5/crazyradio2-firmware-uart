09f55f661ce883448995754231d019d52a626776
Jan Achermann <achermann@campus.tu-berlin.de>
2024-10-29 13:29:39 +0100

    add CONFIG_ESB

--- src/esb.c
+++ src/esb.c
@@ -77,6 +77,7 @@ static uint32_t bytewise_bitswap(uint32_t inp)
 
 static void radio_isr(void *arg)
 {
+    LOG_DBG("Radio INterrupt");
     sending = true;
     ack_enabled = true;
     __NOP();
@@ -130,8 +131,86 @@ static void radio_isr(void *arg)
     nrf_radio_event_clear(NRF_RADIO, NRF_RADIO_EVENT_DISABLED);
     nrf_radio_event_clear(NRF_RADIO, NRF_RADIO_EVENT_END);
 }
+/* Public API */
+
+// S1 is used for compatibility with NRF24L0+. These three bits are used
+// to store the PID and NO_ACK.
+#define PACKET0_S1_SIZE                  (3UL)
+// S0 is not used
+#define PACKET0_S0_SIZE                  (0UL)
+// The size of the packet length field is 6 bits
+#define PACKET0_PAYLOAD_SIZE             (6UL)
+// The size of the base address field is 4 bytes
+#define PACKET1_BASE_ADDRESS_LENGTH      (4UL)
+// Don't use any extra added length besides the length field when sending
+#define PACKET1_STATIC_LENGTH            (0UL)
+// Max payload allowed in a packet
+#define PACKET1_PAYLOAD_SIZE             (63UL)
 
 void esb_init()
+{
+  NRF_RADIO->POWER = 1;
+
+  NRF_RADIO->TXPOWER = (RADIO_TXPOWER_TXPOWER_0dBm << RADIO_TXPOWER_TXPOWER_Pos);
+
+      NRF_RADIO->MODE = (RADIO_MODE_MODE_Nrf_2Mbit << RADIO_MODE_MODE_Pos);
+
+
+  NRF_RADIO->FREQUENCY = 80;
+    static uint64_t address = 0xE7E7E7E7E7ULL;
+  // Radio address config
+  // We use local addresses 0 and 1
+  //  * local address 0 is the unique address of the Crazyflie, used for 1-to-1 communication.
+  //    This can be set dynamically and the current address is stored in EEPROM.
+  //  * local address 1 is used for broadcasts
+  //    This is currently 0xFFE7E7E7E7.
+  NRF_RADIO->PREFIX0 = 0xC4C3FF00UL | (bytewise_bitswap(address >> 32) & 0xFF);  // Prefix byte of addresses 3 to 0
+  NRF_RADIO->PREFIX1 = 0xC5C6C7C8UL;  // Prefix byte of addresses 7 to 4
+  NRF_RADIO->BASE0   = bytewise_bitswap((uint32_t)address);  // Base address for prefix 0
+  NRF_RADIO->BASE1   = 0xE7E7E7E7UL;  // Base address for prefix 1-7
+  NRF_RADIO->TXADDRESS = 0x00UL;      // Set device address 0 to use when transmitting
+  NRF_RADIO->RXADDRESSES = 3;//(1<<0) | (1<<1);    // Enable device address 0 and 1 to use which receiving
+
+  // Packet configuration
+  NRF_RADIO->PCNF0 = (PACKET0_S1_SIZE << RADIO_PCNF0_S1LEN_Pos) |
+                     (PACKET0_S0_SIZE << RADIO_PCNF0_S0LEN_Pos) |
+                     (PACKET0_PAYLOAD_SIZE << RADIO_PCNF0_LFLEN_Pos);
+
+  // Packet configuration
+   NRF_RADIO->PCNF1 = (RADIO_PCNF1_WHITEEN_Disabled << RADIO_PCNF1_WHITEEN_Pos)    |
+                      (RADIO_PCNF1_ENDIAN_Big << RADIO_PCNF1_ENDIAN_Pos)           |
+                      (PACKET1_BASE_ADDRESS_LENGTH << RADIO_PCNF1_BALEN_Pos)       |
+                      (PACKET1_STATIC_LENGTH << RADIO_PCNF1_STATLEN_Pos)           |
+                      (PACKET1_PAYLOAD_SIZE << RADIO_PCNF1_MAXLEN_Pos);
+
+  // CRC Config
+  NRF_RADIO->CRCCNF = (RADIO_CRCCNF_LEN_Two << RADIO_CRCCNF_LEN_Pos); // Number of checksum bits
+  NRF_RADIO->CRCINIT = 0xFFFFUL;      // Initial value
+  NRF_RADIO->CRCPOLY = 0x11021UL;     // CRC poly: x^16+x^12^x^5+1
+
+  // Enable interrupt for end event
+  //NRF_RADIO->INTENSET = RADIO_INTENSET_END_Msk;
+
+  // Set all shorts so that RSSI is measured and only END is required interrupt
+  NRF_RADIO->SHORTS = RADIO_SHORTS_READY_START_Msk;
+  NRF_RADIO->SHORTS |= RADIO_SHORTS_ADDRESS_RSSISTART_Msk;
+  NRF_RADIO->SHORTS |= RADIO_SHORTS_DISABLED_TXEN_Msk;
+  NRF_RADIO->SHORTS |= RADIO_SHORTS_DISABLED_RSSISTOP_Enabled;
+
+
+  // Set RX buffer and start RX
+  
+  NRF_RADIO->PACKETPTR = (uint32_t)ackBuffer;
+  NRF_RADIO->TASKS_RXEN = 1U;
+
+  nrf_radio_int_enable(NRF_RADIO,RADIO_INTENSET_END_Msk);
+    IRQ_CONNECT(RADIO_IRQn, 3, radio_isr, NULL, 0);
+    irq_enable(RADIO_IRQn);
+
+  isInit = true;
+}
+
+/*void esb_init()
 {
     // Timer0
     nrf_timer_bit_width_set(NRF_TIMER0, NRF_TIMER_BIT_WIDTH_32);
@@ -166,14 +245,14 @@ void esb_init()
     nrf_radio_rxaddresses_set(NRF_RADIO, 0x01u);
     
     // Should be correct, doenst work with vanilla 
-    /*uint64_t address = 0xE7E7E7E7E7ULL;
+    uint64_t address = 0xE7E7E7E7E7ULL;
     nrf_radio_base0_set(NRF_RADIO,bytewise_bitswap((uint32_t)address));
     nrf_radio_base1_set(NRF_RADIO, 0xE7E7E7E7UL);
     nrf_radio_prefix0_set(NRF_RADIO,0xC4C3FF00UL | (bytewise_bitswap(address >> 32) & 0xFF));
     nrf_radio_prefix1_set(NRF_RADIO,0xC5C6C7C8UL);
     nrf_radio_txaddress_set(NRF_RADIO, 0);
     nrf_radio_rxaddresses_set(NRF_RADIO, (1<<0) | (1<<1));    
-    */
+    
 
     // Configure CRC
     nrf_radio_crc_configure(NRF_RADIO, 2, NRF_RADIO_CRC_ADDR_INCLUDE, 0x11021UL);
@@ -198,7 +277,7 @@ void esb_init()
     arc = 3;
 
     isInit = true;
-}
+}*/
 
 void esb_deinit()
 {
